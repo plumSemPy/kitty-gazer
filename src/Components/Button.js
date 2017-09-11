14857db3f9d64974f1a4ba73bfdb01718474de3a
plumSemPy <emin@berkeley.edu>
2017-09-10 21:04:18 -0700

    fix the binding issue with the submission button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Button extends Component{
+class Button extends Component {
    constructor(props){
    	super(props);
    	console.log(props)
    	this.state = { 
    		username: props.creds.username,
    		password: props.creds.password
-   	 }
+   	 };
    }
-   submit(){
+   submit = () => {
      // axios.post(
      // 	'http://localhost:3333/api/',
      // 	this.state
      // 	).then((res, rej)=> console.log(res))
      console.log(this.state)
-     console.log(this.props)
    }
    render(){
    	  return <button onClick={this.submit}> Sign Up/Login </button>  
    } 
-
 }
 
 
