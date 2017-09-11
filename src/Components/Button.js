68b7708a1f2e279c0b543482b4487162d4c1e2ac
plumSemPy <emin@berkeley.edu>
2017-09-10 18:07:50 -0700

    WIP submit the form to the api

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Button extends Component{
+   constructor(props){
+   	super(props);
+   	console.log(props)
+   	this.state = { 
+   		username: props.creds.username,
+   		password: props.creds.password
+   	 }
+   }
+   submit(){
+   	console.log(this)
+     axios.post(
+     	'http://localhost:3333/api/',
+     	this.state
+     	).then((res, rej)=> console.log(res))
+   }
    render(){
-   	  return <button> Sign Up/Login </button>  
+   	  return <button onClick={this.submit}> Sign Up/Login </button>  
    } 
 
 }
